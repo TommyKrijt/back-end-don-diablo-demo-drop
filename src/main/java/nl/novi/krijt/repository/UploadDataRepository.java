9653ba2562e0e2d5fcc8c863b6595be82c9b2949
Tommy Krijt <tommykrijt@gmail.com>
2021-02-25 02:02:58 +0100

    small clean up

--- src/main/java/nl/novi/krijt/repository/UploadDataRepository.java
+++ src/main/java/nl/novi/krijt/repository/UploadDataRepository.java
@@ -1,6 +1,5 @@
 package nl.novi.krijt.repository;
 
-import nl.novi.krijt.domain.UploadData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UploadDataRepository extends JpaRepository<UploadData, Long> {
